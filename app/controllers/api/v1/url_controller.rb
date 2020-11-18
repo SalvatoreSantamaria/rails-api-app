389d4845848d738f888ae43bd325c4024f304dbe
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-11-18 14:55:12 -0800

    Returning basic JSON

--- app/controllers/api/v1/url_controller.rb
+++ app/controllers/api/v1/url_controller.rb
@@ -1,7 +1,31 @@
 class Api::V1::UrlController < ApplicationController
 
+  #ONLY USING FOR TEST PURPOSES:
+  skip_before_action :verify_authenticity_token
+  
+  #need to 
+  # give ability to api to take in input. can currently hit route and get back json data.
+  # make new method to get original input back
+  # use database instead of obj
+
   def shortener
-    render json: 'Hello!'
+    @count = 0
+    @dataBase = {}
+    url = 'www.test.com'
+
+    front = "http://shorturl"
+    @count = @count + 1
+    count = @count
+    @dataBase[url] = (front.concat(count.to_s))
+
+
+    #render json: 'Hello!'
+    render json: @dataBase
+
+  end
+end
+
+
   #   class UrlProcess
   #     def initialize 
   #         @count = 0
@@ -29,7 +53,7 @@ class Api::V1::UrlController < ApplicationController
   # puts(url.shortener('www.superextralongurl.com'))
   # puts(url.lengthener('http://shorturl2'))
 
-  end
-end
+
+
 
 #http://localhost:3000/api/v1/url/change/
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'anagram/check'
       get 'url/shortener'
-      #resources :check
+      post 'url/shortener'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
